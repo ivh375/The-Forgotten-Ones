c411a4b1fedc942f2b6b9981419fc33580b029b0
gorilaSam <48575634+gorilaSam@users.noreply.github.com>
2019-10-08 23:43:52 -0600

    Add files via upload

---
+++ Assets/Scripts/BGScroll.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour {
+	
+	public float scroll_Speed = 0.1f;
+	
+	private MeshRenderer mesh_Renderer;
+	private float x_Scroll;
+	
+	void Awake() {
+		mesh_Renderer = GetComponent<MeshRenderer>();
+	}
+	
+	void Update(){
+		Scroll();
+	}
+	
+	void Scroll(){
+		x_Scroll = Time.time * scroll_Speed;
+		Vector2 offset = new Vector2(0f, x_Scroll);
+		mesh_Renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    new Vector2 movement;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
