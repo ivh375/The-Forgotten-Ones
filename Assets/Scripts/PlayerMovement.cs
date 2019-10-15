b1c13bb9ed6b5f5a6684e622dccc05ad155ac176
gorilaSam <48575634+gorilaSam@users.noreply.github.com>
2019-10-14 20:45:56 -0600

    Player movement changed to create player bounds + added light

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-
+    public float speed = 5f;
     public Rigidbody2D rb;
+	public float minY, maxY, minX, maxX;
 
     new Vector2 movement;
     void Start()
@@ -17,11 +17,50 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        MovePlayer();
     }
+	
+	void MovePlayer(){
+		if (Input.GetAxisRaw("Vertical") > 0f) {
+			Vector3 temp = transform.position;
+			temp.y += speed * Time.deltaTime;
+			
+			if (temp.y > maxY)
+				temp.y = maxY;
+			
+			transform.position = temp;
+		} else if (Input.GetAxisRaw("Vertical") < 0f) {
+			Vector3 temp = transform.position;
+			temp.y -= speed * Time.deltaTime;
+			
+			if (temp.y < minY)
+				temp.y = minY;
+			
+			transform.position = temp;
+		}
+		if (Input.GetAxisRaw("Horizontal") > 0f) {
+			Vector3 temp = transform.position;
+			temp.x += speed * Time.deltaTime;
+			
+			if (temp.x > maxX)
+				temp.x = maxX;
+			
+			transform.position = temp;
+		} else if (Input.GetAxisRaw("Horizontal") < 0f) {
+			Vector3 temp = transform.position;
+			temp.x -= speed * Time.deltaTime;
+			
+			if (temp.x < minX)
+				temp.x = minX;
+			
+			transform.position = temp;
+		}
+
+		
+	}
+	
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+		
     }
 }
