c28cb44fa0a166fe5af38232776b6b2fed036fd7
ivh375 <55216992+ivh375@users.noreply.github.com>
2019-10-29 00:40:40 -0600

    Add files via upload
    fixed constant error occurance

--- Assets/Scripts/EnemyCollision.cs
+++ Assets/Scripts/EnemyCollision.cs
@@ -5,27 +5,12 @@ using UnityEngine;
 public class EnemyCollision : MonoBehaviour
 {
     public GameObject bullet;
-    public GameObject enemy;
+    public GameObject Enemy;
     private Collision other;
 
-    private void OnCollisionEnter(Collision other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Enemy"))
-        {
             Destroy(other.gameObject); // this destroys the enemy
-            Destroy(bullet); // this destroys the bullet
-        }
-    }
-
-// Start is called before the first frame update
-void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        OnCollisionEnter(other);
+            Destroy(bullet); // this destroys the bullet        }
     }
 }
