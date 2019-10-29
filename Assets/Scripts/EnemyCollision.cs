c7626e16ef685722c57048d93181479cfb350e85
gorilaSam <48575634+gorilaSam@users.noreply.github.com>
2019-10-29 14:01:39 -0600

    stuff

---
+++ Assets/Scripts/EnemyCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollision : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject Enemy;
+    private Collision other;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+            Destroy(other.gameObject); // this destroys the enemy
+            Destroy(bullet); // this destroys the bullet        }
+    }
+}
