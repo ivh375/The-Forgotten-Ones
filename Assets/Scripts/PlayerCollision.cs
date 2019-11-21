b40023c2630d8c935c7e718cb8a0b78f9d4c6ff3
ivh375 <55216992+ivh375@users.noreply.github.com>
2019-11-20 20:16:18 -0600

    PlayerCollision files and sound files

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+	public PlayerMovement movement;
+
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		if (col.gameObject.tag.Equals ("Enemy"))
+		{
+			SoundManager.PlaySound("ExplosionSound");
+			movement.enabled = false;
+			FindObjectOfType<GameManager>().EndGame();
+		}
+	}
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+	public static AudioClip LaserSound, ExplosionSound;
+	static AudioSource audioSrc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LaserSound = Resources.Load<AudioClip> ("LaserSound");
+        ExplosionSound = Resources.Load<AudioClip> ("ExplosionSound");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void PlaySound(string clip)
+    {
+    	switch(clip)
+    	{
+    		case "LaserSound":
+    			audioSrc.PlayOneShot(LaserSound);
+    			break;
+            case "ExplosionSound":
+                audioSrc.PlayOneShot(ExplosionSound);
+                break;
+    	}
+    }
+}
